92b07416221fe5652473022dbc35569d6c67455c
owhenrique <me.pauloalmeida@gmail.com>
2024-03-28 12:03:17 -0300

    feat: add initial page configs for the template folder

---
+++ src/mocks/aluno.ts
@@ -0,0 +1,10 @@
+const aluno = {
+    foto: "https://xsgames.co/randomusers/assets/avatars/male/46.jpg",
+    sigla: "JD",
+    nome: "Jonh Doe",
+    idade: "33",
+    peso: "75",
+    objetivo: "Hipertrofia"
+}
+
+export default aluno;
