9ad6e40025c3ba3971d72cad462e67e6c217dcac
owhenrique <me.pauloalmeida@gmail.com>
2024-03-31 22:20:36 -0300

    refactor: remove carrosel component

--- src/mocks/treinoA.ts
+++ src/mocks/treinoA.ts
@@ -4,6 +4,7 @@ const treinoA: Treino = {
     categoria: "Peito e Tríceps",
     exercicios: [
       { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2020/12/supino-inclinado-com-halteres.gif", nome: "Supino Inclinado", series: 3, repeticoes: 15, descanso: 60 },
+      { imagem: "https://www.mundoboaforma.com.br/wp-content/uploads/2021/07/triceps-puxada-no-pulley-com-corda.gif", nome: "Tríceps na polia c/ corda", series: 3, repeticoes: 12, descanso: 60 },
     ]
 };
   
